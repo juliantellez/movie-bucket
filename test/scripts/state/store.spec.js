5600c0d0dcd225e78122998bfbb36cf38d721adb
julian <juliantellezmendez@gmail.com>
2017-06-25 23:53:43 +0100

    .

--- test/scripts/state/store.spec.js
+++ test/scripts/state/store.spec.js
@@ -1,7 +1,7 @@
 import store from 'src/state/store'
 
 describe('store', () => {
-  it.only('creates a store', () => {
+  it('creates a store', () => {
     expect(store).to.have.keys('dispatch', 'getState', 'subscribe', 'unsubscribe')
     expect(store.getState()).to.be.an('object')
   })
